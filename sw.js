ee3f5d45e76833167e10ad6d788d9eaa0da5b880
MarkRustenholtz <markrusten@gmail.com>
2025-11-14 00:56:46 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "compt-cache-v16";
+const CACHE_NAME = "compt-cache-v17";
 const urlsToCache = [
   "./",               // page principale
   "./index.html",
@@ -45,6 +45,7 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
 
 
 
