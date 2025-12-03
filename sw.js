b4c2145655a5585e37dfb8f870a59eeb5957eae2
MarkRustenholtz <markrusten@gmail.com>
2025-12-03 01:16:26 +0100

    Update service worker cache version to v30

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "compt-cache-v29";
+const CACHE_NAME = "compt-cache-v30";
 const urlsToCache = [
   "./",               // page principale
   "./index.html",
@@ -45,6 +45,7 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
 
 
 
